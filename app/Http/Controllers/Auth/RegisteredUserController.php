091f71a9099d8816ed242247803a8b794291b862
wtface <nkhpark@hotmail.com>
2025-03-20 15:39:29 +0900

    init

---
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Inertia\Inertia;
+use Inertia\Response;
+
+class RegisteredUserController extends Controller
+{
+  /**
+   * Show the registration page.
+   */
+  public function create(): Response {
+    return Inertia::render('auth/Register');
+  }
+
+  /**
+   * Handle an incoming registration request.
+   *
+   * @throws \Illuminate\Validation\ValidationException
+   */
+  public function store(Request $request): RedirectResponse {
+    $request->validate([
+      'email' => 'required|string|lowercase|email|max:255|unique:' . User::class,
+      'password' => ['required', 'confirmed', 'min:4', 'max:13'],
+    ]);
+
+    $user = User::create([
+      'email' => $request->email,
+      'password' => Hash::make($request->password),
+    ]);
+
+    Auth::login($user);
+
+    return to_route('dashboard');
+  }
+}
