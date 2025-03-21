0465d51bf71d311888e08a4ca589ad0724c4a4f8
wtface <nkhpark@hotmail.com>
2025-03-21 19:55:05 +0900

    main banner1

--- resources/js/components/AppLogo.vue
+++ resources/js/components/AppLogo.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts">
-import logo from '@images/logo.png';
 </script>
 
 <template>
   <Link href="/" class="flex aspect-square size-10 items-center justify-center rounded-md bg-sidebar-primary ">
-    <img :src="logo" alt="logo">
+    <img src="/images/logo.png" alt="logo">
   </Link>
 </template>
