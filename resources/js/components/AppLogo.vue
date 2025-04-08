bbf111bfb46acdb6987c6c2e07f4de308b551a7e
wtface <nkhpark@hotmail.com>
2025-04-08 14:34:57 +0900

    update dark bg, logo

--- resources/js/components/AppLogo.vue
+++ resources/js/components/AppLogo.vue
@@ -2,7 +2,7 @@
 </script>
 
 <template>
-  <Link href="/" class="flex aspect-square size-10 items-center justify-center rounded-md bg-sidebar-primary ">
-    <img src="/images/logo.png" alt="logo">
+  <Link href="/" class="size-10 flex items-center">
+    <img src="/images/logo.png" alt="logo" class="rounded-md">
   </Link>
 </template>
--- resources/js/layouts/app/AppHeaderLayout.vue
+++ resources/js/layouts/app/AppHeaderLayout.vue
@@ -10,12 +10,12 @@ import { Mail, Phone, Printer } from 'lucide-vue-next';
     <main class="container flex h-full flex-1 flex-col rounded-xl">
       <slot />
     </main>
-    <div class="text-sm text-gray-700 dark:text-gray-300 container">
+    <div class="text-sm text-gray-700 dark:text-gray-300 container leading-6">
       <div>
         <span class="text-md font-bold">(사) 국제드림교육원 </span> 고유번호 124-82-352-33 | 대표이사 고홍범
       </div>
       <div>
-        (63110) 제주특별자치도 오광로6길19, 1층 (이호이동)
+        (63110) 제주특별자치도 오광로6길 19, 1층 (이호이동)
       </div>
       <div>
         후원계좌: 농협 301-0366-8740-81
